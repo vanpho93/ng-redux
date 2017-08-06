6cb0c5f92dd90e14f165b4ca2429a9f41458b18f
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-07 00:55:58 +0700

    retrieve data from list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AngularFireAuth, AngularFireDatabase]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  user: Observable<firebase.User>;
+  name: String = 'Nguyen Van Pho';
+  items: FirebaseListObservable<any[]>;
+  constructor(private db: AngularFireDatabase, private auth: AngularFireAuth) {}
+
+  ngOnInit(): void {
+    this.items = this.db.list('/message');
+    this.user = this.auth.authState;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBeEmU61t-41704qEivDriBaniTzZMLMRk',
+  authDomain: 'ng-chat-243a5.firebaseapp.com',
+  databaseURL: 'https://ng-chat-243a5.firebaseio.com',
+  projectId: 'ng-chat-243a5',
+  storageBucket: 'ng-chat-243a5.appspot.com',
+  messagingSenderId: '806532972203'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
